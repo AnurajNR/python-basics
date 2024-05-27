b4ef960ea5e00e930dc2b01e517a8c79c90f6b17
AnurajNR <anurajraj4158@gmail.com>
2024-05-27 14:35:23 -0700

    modules

---
+++ modules.py
@@ -0,0 +1,17 @@
+import summodule
+import math
+import random
+print(summodule.add(2,3))
+#substraction
+print(summodule.sub(2,3))
+print(min(2,4,6,8,))
+print(max(3,6,9,12))
+print(abs(-3))
+print(pow(4,2))
+print(math.sqrt(25))
+print(math.pi)
+print(math.factorial(10))
+print(math.sin(2))
+print(math.cos(2))
+print(math.tan(2))
+print(random.random())
---
+++ summodule.py
@@ -0,0 +1,5 @@
+#python functions
+def add(a,b):
+    return(a+b)
+def sub(a,b):
+    return(a-b)
